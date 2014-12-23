aac9af0667b959c8240d4fc68558ce9300ecf9fa
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-12-23 11:05:36 -0800

    Add some basic tests, also add framework to test multiple sort fns at once

--- test/insertionSortTest.js
+++ test/insertionSortTest.js
@@ -1,6 +1,35 @@
 
-describe('Insertion Sort', function() {
-  it('should sort in ascending order by default', function() {
-    expect(true).toBe(true);
+var sortFns = [
+  {
+    name: 'insertionSort',
+    fn: insertionSort,
+    stable: true
+  }
+];
+
+// Tests all the sort functions at once
+for (var i=0; i<sortFns.length; i++) {
+  describe(sortFns[i].name, function() {
+
+    var sortFn = sortFns[i].fn;
+
+    it('should be a function', function() {
+      expect(typeof sortFn).toBe('function');
+    });
+    
+    it('should sort in ascending order by default', function() {
+      expect(sortFn('cabd')).toBe('abcd');
+    });
+    
+    it('should be able to sort arrays', function() {
+      expect(sortFn(['hi', 'there', 'everyone'])).toEqual(['everyone', 'hi', 'there']);
+    });
+
+    it('should not sort arrays in-place', function() {
+      var arr = ['hi', 'there', 'everyone'];
+      expect(sortFn(arr)).not.toBe(arr);
+    });
+
   });
-});
+  
+}
